06bbd2e5fde7b6cc99630bde2e81c0f3b66271ad
Nicholas Forystek <110750650+nforystek@users.noreply.github.com>
2023-03-20 01:31:35 -0700

    Updates

--- Source/Max-FTP/Groups/Test/SetService_Dev.bat
+++
@@ -1,4 +0,0 @@
-..\Binary\MaxUtility.exe /stop
-..\Binary\MaxService.exe uninstall
-..\Binary\MaxService.exe install 0
-..\Binary\MaxUtility.exe /start
--- Source/Max-FTP/Groups/Test/SetService_Lend.bat
+++
@@ -1,4 +0,0 @@
-"C:\Program Files\Max-FTP\MaxUtility.exe" /stop
-"C:\Program Files\Max-FTP\MaxService.exe" uninstall
-"C:\Program Files\Max-FTP\MaxService.exe" install 0
-"C:\Program Files\Max-FTP\MaxUtility.exe" /start
